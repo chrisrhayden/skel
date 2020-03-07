fa95b4f8b7167c4ee7b984f0274e5ed54a1a696f
chris <chrisrhayden@gmail.com>
2020-03-07 13:20:43 -0800

    add fucntion

--- src/template.rs
+++ src/template.rs
@@ -1,6 +1,10 @@
-// this is terrible
+// these fucking suck
 pub fn template(root: &str, name: &str, old_string: &str) -> String {
     let new_string = old_string.replace("{{root}}", root);
 
     new_string.replace("{{name}}", name)
+}
+
+pub fn template_str(user_str: &str, old_string: &str) -> String {
+    old_string.replace("{{config-new}}", user_str)
 }
