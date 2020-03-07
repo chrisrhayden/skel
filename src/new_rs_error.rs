8bed8eb717697e79b468dbcfb0ccb7a02c8977d9
chris <chrisrhayden@gmail.com>
2020-03-07 10:07:17 -0800

    code clean up

--- src/new_rs_error.rs
+++ src/new_rs_error.rs
@@ -1,17 +1,17 @@
-use std::fmt;
+use std::{error::Error, fmt};
 
 // basic error type enum to pattern match on
-#[derive(Debug, Clone, PartialEq)]
-pub enum NewRsErrorType {
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum NewInnerErrType {
     ProjectExists,
     IoError,
 }
 
-impl fmt::Display for NewRsErrorType {
+impl fmt::Display for NewInnerErrType {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let message = match self {
-            NewRsErrorType::ProjectExists => String::from("ProjectExists"),
-            NewRsErrorType::IoError => String::from("IoError"),
+            NewInnerErrType::ProjectExists => String::from("ProjectExists"),
+            NewInnerErrType::IoError => String::from("IoError"),
         };
 
         write!(f, "{}", message)
@@ -21,23 +21,32 @@ impl fmt::Display for NewRsErrorType {
 // a basic error for new_rs
 // TODO: make this use an error trait
 #[derive(Debug)]
-pub struct NewRsError {
+pub struct NewInnerError {
     err_str: String,
-    err_type: NewRsErrorType,
+    err_type: NewInnerErrType,
 }
 
-impl NewRsError {
-    pub fn new(err_type: NewRsErrorType, err_str: String) -> Self {
+impl NewInnerError {
+    pub fn new(err_type: NewInnerErrType, err_str: String) -> Self {
         Self { err_type, err_str }
     }
 
+    pub fn from_io_err(io_err: Box<dyn Error>) -> NewInnerError {
+        let err_string = format!("{:?}", io_err);
+
+        let err_type = NewInnerErrType::IoError;
+
+        NewInnerError::new(err_type, err_string)
+    }
+
+    // this is only used in test at the moment
     #[allow(dead_code)]
-    pub fn kind(&self) -> NewRsErrorType {
-        self.err_type.clone()
+    pub fn kind(&self) -> NewInnerErrType {
+        self.err_type
     }
 }
 
-impl fmt::Display for NewRsError {
+impl fmt::Display for NewInnerError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(
             f,
