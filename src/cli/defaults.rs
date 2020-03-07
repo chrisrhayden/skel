0290ba05c0c46a6721ec4be69844233bd92e8e5e
chris <chrisrhayden@gmail.com>
2020-03-07 10:02:15 -0800

    larg clean up of bad / redunden code

--- src/cli/defaults.rs
+++ src/cli/defaults.rs
@@ -4,8 +4,7 @@ use serde::Deserialize;
 
 use crate::{collect_project_config, Project};
 
-use super::my_utils::get_home_dir;
-use super::NewArgs;
+use super::parse_args::NewArgs;
 
 pub type NewResult<T> = Result<T, Box<dyn Error>>;
 pub type UserResult = NewResult<UserConfig>;
@@ -16,36 +15,63 @@ pub struct UserConfig {
     pub alias: HashMap<String, Vec<String>>,
 }
 
+fn find_project_file(
+    user_config: UserConfig,
+    type_str: String,
+) -> NewResult<String> {
+    let type_string = type_str.to_string();
+
+    if let Some(path_string) = user_config.projects.get(&type_string) {
+        return Ok(path_string.clone());
+    }
+
+    let mut project_string = String::new();
+
+    for (project, alias) in user_config.alias.iter() {
+        if alias.contains(&type_string) {
+            project_string.clone_from(project);
+            break;
+        }
+    }
+
+    if project_string.is_empty() {
+        return Err(Box::from(format!(
+            "given project type not in user config -- {}",
+            type_str
+        )));
+    }
+
+    match user_config.projects.get(&project_string) {
+        Some(val) => Ok(val.to_string()),
+        None => {
+            // this seems unlikely
+            Err(Box::from(format!(
+                "no project for that alias -- {}",
+                type_string
+            )))
+        }
+    }
+}
+
 fn template_user_config(user_str: &str, old_string: &str) -> String {
     old_string.replace("{{new-config}}", user_str)
 }
 
 fn project_path_with_templateing(
-    args: &NewArgs,
-    user_config: &UserConfig,
-    config_dir: &Option<String>,
+    type_str: String,
+    user_config: UserConfig,
+    config_dir: String,
 ) -> NewResult<PathBuf> {
-    let project_pathbuf = if let Some(proj_str) = args.type_str.as_ref() {
-        let p_string = find_project_file(user_config, &proj_str)?;
+    let p_string = find_project_file(user_config, type_str)?;
 
-        let p_string = if let Some(c_dir) = config_dir {
-            template_user_config(c_dir, &p_string)
-        } else {
-            panic!("did not get project_dir string");
-        };
+    let p_string = template_user_config(&config_dir, &p_string);
 
-        PathBuf::from(p_string)
-    } else {
-        // we should not reach this
-        panic!("did not receive the project type or file");
-    };
-
-    Ok(project_pathbuf)
+    Ok(PathBuf::from(p_string))
 }
 
-fn root_string_default_or(root: &Option<String>) -> String {
-    if let Some(root) = root {
-        root.to_owned()
+fn root_string_default_or(root_from_cli: &Option<String>) -> String {
+    if let Some(from_cli) = root_from_cli {
+        from_cli.to_owned()
     } else {
         // default to current_dir
         env::current_dir()
@@ -56,34 +82,28 @@ fn root_string_default_or(root: &Option<String>) -> String {
     }
 }
 
+// TODO: make the path delimiter and config name variables
 fn config_string_default_or(
-    config_path: &Option<String>,
-) -> (String, Option<String>) {
-    if let Some(config_path) = config_path {
-        (config_path.to_owned(), None)
-    } else {
-        let mut config_dir = get_home_dir();
-        config_dir.push(".config");
-        config_dir.push("new");
-        let mut config_path = config_dir.clone();
-        config_path.push("new_config.toml");
+    config_path_from_cli: Option<String>,
+) -> (String, String) {
+    let mut config_dir = match config_path_from_cli {
+        Some(config_path) => config_path.clone(),
+        None => env::var("HOME").expect("cant get env var HOME"),
+    };
 
-        (
-            config_path
-                .to_str()
-                .expect("cant make str from default config  file")
-                .to_owned(),
-            Some(
-                config_dir
-                    .to_str()
-                    .expect("cant make str from default config  file")
-                    .to_owned(),
-            ),
-        )
-    }
+    // first make the directory
+    config_dir.push_str(".config");
+    config_dir.push('/');
+    config_dir.push_str("new");
+
+    // then the actual file
+    let mut config_path = config_dir.clone();
+    config_path.push_str("config.toml");
+
+    (config_path, config_dir)
 }
 
-fn get_user_config(config_str: &str) -> UserResult {
+fn make_config_from_toml(config_str: &str) -> UserResult {
     use std::io::Read;
 
     let config_path = PathBuf::from(config_str);
@@ -112,71 +132,36 @@ fn get_user_config(config_str: &str) -> UserResult {
     Ok(toml_conf)
 }
 
-pub fn config_str_to_user_struct(
-    config_path: &Option<String>,
-) -> NewResult<(UserConfig, Option<String>)> {
+pub fn collect_user_config(
+    config_path: Option<String>,
+) -> NewResult<(UserConfig, String)> {
     let (u_s, u_d) = config_string_default_or(config_path);
 
-    let conf = get_user_config(&u_s)?;
+    let conf = make_config_from_toml(&u_s)?;
 
     Ok((conf, u_d))
 }
 
-fn find_project_file(
-    user_config: &UserConfig,
-    type_str: &str,
-) -> NewResult<String> {
-    let type_string = type_str.to_string();
-
-    if let Some(path_string) = user_config.projects.get(&type_string) {
-        return Ok(path_string.clone());
-    }
-
-    let mut project_string = String::new();
-    for (project, alias) in user_config.alias.iter() {
-        // println!("--->>>>> {:?} {:?}", project, alias);
-        if alias.contains(&type_string) && project_string.is_empty() {
-            project_string.push_str(project);
-        }
-    }
-
-    if project_string.is_empty() {
-        return Err(Box::from(format!(
-            "given project type not in user config -- {}",
-            type_str
-        )));
-    }
-
-    match user_config.projects.get(&project_string) {
-        Some(val) => Ok(val.to_string()),
-        None => {
-            // this seams unlikely
-            Err(Box::from(format!(
-                "no project for that ailas -- {}",
-                type_string
-            )))
-        }
-    }
-}
-
-// last takes precedents:
+// last takes precedent:
 //      default > config > cli config
-pub fn resolve_default(
-    args: &NewArgs,
-    user_config: &UserConfig,
-    config_dir: &Option<String>,
-) -> NewResult<Project> {
-    // if we dont have a name here we never will
-    let name = match args.name.clone() {
-        None => return Err(Box::from(String::from("did not get name"))),
-        Some(val) => val,
+pub fn resolve_default(args: NewArgs) -> NewResult<Project> {
+    let name = args.name;
+
+    let project_pathbuf = match args.cli_project_file {
+        Some(project_file) => PathBuf::from(project_file),
+        None => {
+            let (user_config, config_dir_path) =
+                collect_user_config(args.cli_config_path)?;
+
+            project_path_with_templateing(
+                args.type_str,
+                user_config,
+                config_dir_path,
+            )?
+        }
     };
 
-    let project_pathbuf =
-        project_path_with_templateing(&args, user_config, config_dir)?;
-
-    // return if project path dose not exists, we will be able to make projects
-    // from directory's eventually
+    // return if project path dose not exists
     if !project_pathbuf.exists() {
         return Err(Box::from(format!(
             "project path given dos not exists -- {}",
@@ -186,7 +171,7 @@ pub fn resolve_default(
 
     let project_config = collect_project_config(&project_pathbuf)?;
 
-    let mut root_string = root_string_default_or(&args.root);
+    let mut root_string = root_string_default_or(&args.different_root);
     // set root to the project name not the current_dir
     // or the one given on the cli
     // TODO: make this generic for windows maybe
