063558c198a1808c41b67b4f8e5663d5511a0e5c
chrisrhayden <chrisrhayden@gmail.com>
2021-01-29 05:27:32 -0800

    rename root_path to project_root_path for clarity

--- src/template.rs
+++ src/template.rs
@@ -1,13 +1,14 @@
 #[derive(Debug)]
 pub struct TemplateArgs<'a> {
-    pub root_path: &'a str,
     pub project_name: &'a str,
+    pub project_root_path: &'a str,
     pub skel_config_path: &'a str,
 }
 
 // these fucking suck
 pub fn template(template_view: &TemplateArgs, old_string: &str) -> String {
-    let new_string = old_string.replace("{{root}}", template_view.root_path);
+    let new_string =
+        old_string.replace("{{root}}", template_view.project_root_path);
 
     let new_string =
         new_string.replace("{{config-dir}}", template_view.skel_config_path);
