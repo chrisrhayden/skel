10026a35ef1bed616ca5fa73d6e8c693e93c0c4d
chrisrhayden <chrisrhayden@gmail.com>
2022-03-31 11:01:51 -0700

    Updating args file for developing

--- src/parse_args.rs
+++ src/parse_args.rs
@@ -1,20 +1,20 @@
-#[derive(Default)]
+#[derive(Default, Debug)]
 pub struct SkelArgs {
     pub name: String,
     pub main_config_path: Option<String>,
     pub skeleton: Option<String>,
     pub skeleton_file: Option<String>,
     pub different_root: Option<String>,
-    pub dry_run: Option<bool>,
+    pub dry_run: bool,
 }
 
 // NOTE: this is a test function
 pub fn make_args() -> SkelArgs {
     SkelArgs {
-        main_config_path: None,
+        main_config_path: Some("./docs/example.config.toml".into()),
         skeleton: Some(String::from("py")),
         name: String::from("fuck"),
-        dry_run: Some(true),
+        dry_run: true,
         ..Default::default()
     }
 }
