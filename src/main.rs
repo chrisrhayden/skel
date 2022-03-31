baf38390fd0bfe67080ea44a505a7364a7b8374f
chrisrhayden <chrisrhayden@gmail.com>
2022-03-31 11:01:27 -0700

    add get_root_path for main

--- src/main.rs
+++ src/main.rs
@@ -3,33 +3,67 @@ mod parse_args;
 mod project_tree;
 mod templating;
 
-use std::process;
+#[cfg(test)]
+mod test_utils;
+
+use std::{
+    env::current_dir, error::Error, fs::metadata, path::PathBuf, process,
+};
+
+use crate::parse_args::SkelArgs;
+
+fn get_root(args: &SkelArgs) -> Result<PathBuf, Box<dyn Error>> {
+    if let Some(diff_root) = &args.different_root {
+        let diff_root_path = PathBuf::from(diff_root);
+
+        if diff_root_path.is_dir() {
+            Ok(diff_root_path)
+        } else {
+            Err(Box::from(format!(
+                "different root does not exists or is not a dir {}",
+                diff_root
+            )))
+        }
+    } else {
+        let root_path = current_dir().expect("could not get current dir");
+
+        let root_string = root_path
+            .to_str()
+            .expect("could not parse root string")
+            .into();
+
+        Ok(root_string)
+    }
+}
 
 // the real `main()` so we can clean up before `process::exit()`
-fn run() -> bool {
+fn run() -> Result<(), Box<dyn Error>> {
     let args = parse_args::make_args();
 
-    let config = match config::resolve_config(&args) {
-        Err(err) => {
-            eprintln!("{}", err);
-            return false;
-        }
-        Ok(config) => config,
-    };
+    let mut root_string = get_root(&args)?;
 
-    match project_tree::make_project_tree(&args, &config) {
-        Err(err) => {
-            eprintln!("{}", err);
-            false
-        }
-        Ok(_) => true,
+    root_string.push(&args.name);
+
+    if !args.dry_run && metadata(&root_string).is_ok() {
+        return Err(Box::from(format!(
+            "project exists {}",
+            root_string.as_os_str().to_str().unwrap()
+        )));
     }
+
+    let config = config::resolve_config(&args, root_string)?;
+
+    project_tree::make_project_tree(&args, &config)
 }
 
 /// this wraps `run()` so everything can be cleaned up before exiting with an error
 /// code
 fn main() {
-    if !run() {
+    if let Err(err) = run() {
+        eprintln!("{}", err);
+
         process::exit(1);
     }
+
+    process::exit(0);
 }
