a253df0f0ceaa8a290694e777c3c5850ba5ae9ad
chrisrhayden <chrisrhayden@gmail.com>
2022-04-06 10:55:38 -0700

    adding clap to parse args

--- src/parse_args.rs
+++ src/parse_args.rs
@@ -1,20 +1,53 @@
-#[derive(Default, Debug)]
+use std::error::Error;
+
+use clap::Parser;
+
+#[derive(Parser, Default, Debug)]
 pub struct SkelArgs {
-    pub name: String,
-    pub main_config_path: Option<String>,
     pub skeleton: Option<String>,
+    pub name: Option<String>,
+    #[clap(short, long)]
     pub skeleton_file: Option<String>,
+    #[clap(short, long)]
+    pub alt_config_path: Option<String>,
+    #[clap(short = 'D', long)]
     pub different_root: Option<String>,
+    #[clap(short, long)]
     pub dry_run: bool,
 }
 
 // NOTE: this is a test function
 pub fn make_args() -> SkelArgs {
     SkelArgs {
-        main_config_path: Some("./docs/example.config.toml".into()),
+        alt_config_path: Some("./docs/example.config.toml".into()),
         skeleton: Some(String::from("py")),
-        name: String::from("fuck"),
+        name: Some(String::from("fuck")),
         dry_run: true,
         ..Default::default()
     }
+}
+
+pub fn parse_args() -> Result<SkelArgs, Box<dyn Error>> {
+    let mut skel_args = SkelArgs::parse();
+
+    if skel_args.skeleton.is_none() && skel_args.skeleton_file.is_none() {
+        return Err(Box::from(String::from(
+            "did not get a skeleton or skeleton-file to make",
+        )));
+    }
+
+    if skel_args.name.is_none()
+        && (skel_args.skeleton.is_some() && skel_args.skeleton_file.is_some())
+    {
+        skel_args.name = Some(skel_args.skeleton.as_ref().unwrap().to_owned());
+    }
+
+    if skel_args.name.is_some()
+        && skel_args.skeleton_file.is_some()
+        && skel_args.skeleton.is_some()
+    {
+        return Err(Box::from(String::from("did not get correct args")));
+    }
+
+    Ok(skel_args)
 }
