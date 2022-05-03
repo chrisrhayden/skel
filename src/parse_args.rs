d7796bd7e4e770f26e581babb910eb73f40fa75c
chrisrhayden <chrisrhayden@gmail.com>
2022-05-03 11:45:17 -0700

    add descriptions to arguments

--- src/parse_args.rs
+++ src/parse_args.rs
@@ -2,17 +2,24 @@ use std::error::Error;
 
 use clap::Parser;
 
+/// make a project from a skeleton defined in a toml file
 #[derive(Parser, Default, Debug)]
 pub struct SkelArgs {
+    /// the skeleton to make, can be the skeleton name or alias
     pub skeleton: Option<String>,
+    /// the name of the new project to make
     pub name: Option<String>,
     #[clap(short, long)]
+    /// a path to a skeleton file
     pub skeleton_file: Option<String>,
     #[clap(short, long)]
+    /// a path to a main config file
     pub alt_config_path: Option<String>,
     #[clap(short = 'D', long)]
+    /// a different root to make the project in to
     pub different_root: Option<String>,
     #[clap(short, long)]
+    /// print out what will be done
     pub dry_run: bool,
 }
 
@@ -51,7 +58,7 @@ pub fn parse_args() -> Result<SkelArgs, Box<dyn Error>> {
     }
 
     // if `name` is none and `skeleton` and `skeleton_file` is some then the
-    // `skeleton` variable is where `name` is
+    // `skeleton` variable is the `name`
     if skel_args.name.is_none()
         && (skel_args.skeleton.is_some() && skel_args.skeleton_file.is_some())
     {
